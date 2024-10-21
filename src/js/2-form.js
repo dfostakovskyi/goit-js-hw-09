53f42db44eb120a2a0acb1903ce0f8044aa8d3d6
Dfostakovskyi <d.fostakovskiy@gmail.com>
2024-10-20 20:39:26 -0700

    change linc

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,42 @@
 "use strict";
+
+let formData = { email: "", message: "" };
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+
+const savedData = localStorage.getItem("feedback-form-state");
+if (savedData) {
+  formData = JSON.parse(savedData);
+  emailInput.value = formData.email || "";
+  messageInput.value = formData.message || "";
+}
+
+const saveToLocalStorage = () => {
+  localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+};
+
+form.addEventListener('input', (event) => {
+  const { name, value } = event.target;
+  formData[name] = value; 
+  saveToLocalStorage(); 
+});
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); 
+
+
+if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+
+  console.log(formData);
+
+
+  localStorage.removeItem("feedback-form-state");
+  formData = { email: "", message: "" }; 
+  form.reset(); 
+});
